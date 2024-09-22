141d7e39ed2f9325b21a66918303ec8848155d58
Jewel Camanso <camansojewel18@gmail.com>
2024-09-22 18:56:33 +0800

    updated comments

--- Diamond_Shape.py
+++ Diamond_Shape.py
@@ -1,18 +1,20 @@
 def print_diamond(n):
-    # Ask the user to input an odd interger
+    # Ask the user to input an odd interger, if n is even, print a message and exit the function
     if n % 2 == 0:
         print("Please provide an odd integer.")
         return
 
-    # Top half including the middle row
+    # Top half including the middle row of the diamond
     for i in range(n // 2 + 1):
+        # Print spaces to center the stars, then print stars in increasing numbers
         print(' ' * (n // 2 - i) + '*' * (2 * i + 1))
 
-    # Bottom half
+    # Bottom half of the diamond
     for i in range(n // 2 - 1, -1, -1):
+        # Print spaces to center the stars, then print stars in decreasing numbers
         print(' ' * (n // 2 - i) + '*' * (2 * i + 1))
 
 
-# Example usage
+# Example usage. Prompt user for input
 n = int(input("Enter an odd integer: "))
 print_diamond(n)
